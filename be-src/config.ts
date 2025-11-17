1730c72eb93cab6628d26402dd97c424dd8f99f1
cpl010 <caiopuyolleguiza@gmail.com>
2025-11-17 20:48:07 -0300

    <chore>(config.ts, html-middleware):Se agrego la variable de entorno de resend.

--- be-src/config.ts
+++ be-src/config.ts
@@ -6,9 +6,10 @@ const PORT = process.env.PORT || 8080;
 const SECRET = process.env.SECRETWORD!;
 const API_KEY = process.env.API_KEY!;
 const APP_ID = process.env.APP_ID!;
-const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME!
-const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY!
-const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET!
+const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME!;
+const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY!;
+const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET!;
+const RESEND_API_KEY = process.env.RESEND_API_KEY!;
 
 if (!SECRET) throw new Error("Missing SECRETWORD");
 if (!API_KEY) throw new Error("Missing API_KEY");
@@ -16,6 +17,7 @@ if (!APP_ID) throw new Error("Missing APP_ID");
 if (!CLOUDINARY_CLOUD_NAME) throw new Error("Missing CLOUDINARY_CLOUD_NAME");
 if (!CLOUDINARY_API_KEY) throw new Error("Missing CLOUDINARY_API_KEY");
 if (!CLOUDINARY_API_SECRET) throw new Error("Missing CLOUDINARY_API_SECRET");
+if (!RESEND_API_KEY) throw new Error("Missing RESEND_API_KEY");
 
 export {
   SECRET,
@@ -24,5 +26,6 @@ export {
   APP_ID,
   CLOUDINARY_CLOUD_NAME,
   CLOUDINARY_API_KEY,
-  CLOUDINARY_API_SECRET
+  CLOUDINARY_API_SECRET,
+  RESEND_API_KEY
 };
